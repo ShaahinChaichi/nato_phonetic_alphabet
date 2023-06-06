dd7b49c2fbc9c143af874f5a287765f112cfeb3f
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-06-06 20:08:40 +0330

    Project finished

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pandas
 
 
-def start():
+def phonetic_generator():
     data = pandas.read_csv("nato_phonetic_alphabet.csv")
     phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
     word = input("Enter a word: ").upper()
     try:
         output_list = [phonetic_dict[letter] for letter in word]
-        print(output_list)
     except KeyError:
         print("Please Try Again!!!")
-        start()
+        phonetic_generator()
+    else:
+        print(output_list)
 
 
-start()
+phonetic_generator()
